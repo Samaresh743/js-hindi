09de2a04429cab4e7e75267960a64abf2f66ca83
Samaresh743 <samareshbarui29@gmail.com>
2025-01-06 18:25:18 +0000

    done

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,2 +1,8 @@
-console.log("hi i am samaresh")
-console.log("hi i am 22")
+const accountId = 144553
+let accountEmail = "hitesh@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+//accountId = 2  //not allowed
+
+console.log(accountId)
