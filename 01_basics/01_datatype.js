0187441244e7dd0514f5fc91369947c2376a8280
Samaresh743 <samareshbarui29@gmail.com>
2025-01-08 10:37:01 +0000

    datatype

---
+++ 01_basics/01_datatype.js
@@ -0,0 +1,16 @@
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+//number
+//bigint
+//string
+//boolean => true/false
+//null => standalone value
+//undefined => 
+    //symbol  => unique
+
+//object
+
+console.log(typeof undefined);
